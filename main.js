70f39d217cab50f1147bf3ef3653c8d50fb1b159
Kiefer Lam <kieferlam@gmail.com>
2020-09-17 20:22:44 +0100

    Set BOIDS_PATH variable for fetch

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import * as Boids from './boids/boid_canvas.js';
 
 var canvas = document.getElementById('render-canvas');
 
+window.BOIDS_PATH = '.';
+
 Boids.setupBoidCanvas(canvas);
 
 Boids.boid_main();
